e430346611ac0a0627cf56d99c995256cbe11ab6
AnisSalhi <Anis_freestyle@hotmail.com>
2017-10-21 21:40:17 +0100

    add test of switchElements methode

--- src/test/main/VectorHelperTest.java
+++ src/test/main/VectorHelperTest.java
@@ -26,4 +26,21 @@ public class VectorHelperTest {
         VectorHelper.sortVector(a);
         for(int i=0; i<=3; i++) assertEquals((int)a.coordinates.get(i),test1[i]);
     }
+
+    @Test
+    public void switchElementsTest() throws Exception {
+        ArrayList<Integer> test= new ArrayList<>();
+        test.add(0,1);
+        test.add(1,8);
+        test.add(2,9);
+        test.add(3,7);
+        int [] test1=new int[4];
+        test1[0]=7;
+        test1[1]=9;
+        test1[2]=8;
+        test1[3]=1;
+        Vector a=new Vector(test,4);
+        VectorHelper.switchElements(a);
+        for(int i=0; i<=3; i++) assertEquals((int)a.coordinates.get(i),test1[i]);
+    }
 }
