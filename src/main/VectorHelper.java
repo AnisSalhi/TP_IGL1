8492b41d87c99ce4969c6ac7a0f7a6add2ab721a
AnisSalhi <Anis_freestyle@hotmail.com>
2017-10-21 21:07:10 +0100

    Initial commit

---
+++ src/main/VectorHelper.java
@@ -0,0 +1,8 @@
+package main;
+
+/**
+ * Created by Lenovo on 21/10/2017.
+ */
+public class VectorHelper {
+
+}
---
+++ src/test/main/VectorHelperTest.java
@@ -0,0 +1,10 @@
+package main;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lenovo on 21/10/2017.
+ */
+public class VectorHelperTest {
+
+}
